0108fc641e592a8788d297b9afa952ccadfbac5f
marvin evins <maevins@cps.edu>
2018-11-09 15:15:22 -0600

    improving interface

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import {Consumer} from '../../context';
-/**
- * Tracks
- */
+import Spinner from '../layout/Spinner';
+import Track from '../tracks/Track';
+
 export class Tracks extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <Consumer>
         {value => {
-          console.log(value);
-          return <h1>Tracks</h1>
-          
+          //console.log(value);
+          const {track_list,heading} = value;
+        if(track_list == undefined || track_list==0){
+            return <Spinner />
+        }else{
+              return (
+                <React.Fragment>
+                <h3 className= "text-center mb-4"> {heading}</h3>
+
+                  <div className="row">
+                      {track_list.map(item => (
+                        <Track key={item.track.track_id} track = {item.track} />
+                      ))}
+                  </div>
+                </React.Fragment>
+              )
+        }
+
         }}
       </Consumer>
     );
