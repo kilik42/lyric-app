7744cea9ee2dab7c43f94a6a67c6c8e4d4d74cc0
marvin evins <maevins@cps.edu>
2018-11-09 07:04:46 -0600

    lyric app first commit

---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+/**
+ * Tracks
+ */
+export class Tracks extends Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <h1> Tracks </h1>
+      </div>
+    );
+  }
+}
+
+
+export default Tracks;
